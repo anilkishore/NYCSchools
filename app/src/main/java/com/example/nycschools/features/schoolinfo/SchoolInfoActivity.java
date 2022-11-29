787d453a59df25c9a64c8e1a49f7865eeaa6e327
Anil Kishore <anil.daiict@gmail.com>
2022-11-29 10:40:33 -0800

    email, phone
    Add email and dial phone actions

--- app/src/main/java/com/example/nycschools/features/schoolinfo/SchoolInfoActivity.java
+++ app/src/main/java/com/example/nycschools/features/schoolinfo/SchoolInfoActivity.java
@@ -1,6 +1,9 @@
 package com.example.nycschools.features.schoolinfo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,8 +33,29 @@ public class SchoolInfoActivity extends AppCompatActivity {
 
         ((TextView) findViewById(R.id.name)).setText(schoolProfile.getSchoolName());
         ((TextView) findViewById(R.id.city)).setText(schoolProfile.getCity());
+
         ((TextView) findViewById(R.id.email)).setText(schoolProfile.getSchoolEmail());
-        ((TextView) findViewById(R.id.description)).setText("<b>About</b>: \n" + schoolProfile.getOverviewParagraph());
+        ((TextView) findViewById(R.id.email)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:" + schoolProfile.getSchoolEmail()));
+                v.getContext().startActivity(Intent.createChooser(intent, "Send Email"));
+            }
+        });
+
+        ((TextView) findViewById(R.id.phone)).setText("Phone: " + schoolProfile.getPhoneNumber());
+        ((TextView) findViewById(R.id.phone)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + schoolProfile.getPhoneNumber()));
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+        ((TextView) findViewById(R.id.description)).setText("About: \n" + schoolProfile.getOverviewParagraph());
 
         SATScoresViewModel viewModel = new ViewModelProvider(this).get(SATScoresViewModel.class);
         LiveData<SchoolSATScores> satScores = viewModel.getSATScores(schoolProfile);
